e335f00829d8e1312eb67bf21da8118d540b7540
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-10-10 17:44:33 +0600

    favorite card section added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 const tagName = document.getElementsByTagName('html')[0];
-tagName.setAttribute("data-theme" , "light")
+tagName.setAttribute("data-theme" , "light");
+
+const favoriteCard = document.getElementById('favorite-card');
